66b8fd390a07886704d4917020f845de9d5bcde5
chris-carpenter <carpycarp44@gmail.com>
2022-12-08 16:53:19 -0500

    Basic functions I made included in the js file. View with console.

---
+++ script.js
@@ -0,0 +1,25 @@
+function add7(number){
+    return (number + 7);
+}
+
+console.log(add7(7));
+
+function multiply(number1, number2){
+    return (number1 * number2);
+}
+
+console.log(multiply(7, 7));
+
+function capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1,).toLowerCase();
+}
+
+console.log(capitalize('hello'));
+
+
+
+function lastLetter(string2){
+    return string2.charAt(string2.length - 1);
+}
+
+console.log(lastLetter('Chowbearapopomaus'));
